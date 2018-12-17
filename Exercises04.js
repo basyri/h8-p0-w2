e4b9992e392bb33fc56f887d5b9187de43e06938
basyri <husain.basyri@gmail.com>
2018-12-18 03:26:27 +0700

    Update Exercises04

--- Exercises04.js
+++ Exercises04.js
@@ -1,13 +1,49 @@
-var tanggal = 12 // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
-var bulan = 4 // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
-var tahun = 1995 // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+var tanggal = 12; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
+var bulan = 2; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var tahun = 1995; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
 
-switch(tanggal && bulan && tahun){
-    case 12:
+switch(bulan){
+    case 1:
+        console.log(tanggal,'Januari', tahun);
+        break;
+    case 2:
+        console.log(tanggal,'Februari', tahun);
+        break;
+    case 3:
+        console.log(tanggal,'Maret', tahun);
+        break;
     case 4:
-    case 1995:
-        console.log('12 April 1995')
-        break
+        console.log(tanggal,'April', tahun);
+        break;
+    case 5:
+        console.log(tanggal,'Mei', tahun);
+        break;
+    case 6:
+        console.log(tanggal,'Juni', tahun);
+        break;
+    case 7:
+        console.log(tanggal,'Juli', tahun);
+        break;
+    case 8:
+        console.log(tanggal,'Agustus', tahun);
+        break;
+    case 9:
+        console.log(tanggal,'September', tahun);
+        break;
+    case 10:
+        console.log(tanggal,'Oktober', tahun);
+        break;
+    case 11:
+        console.log(tanggal,'November', tahun);
+        break;
+    case 12:
+        console.log(tanggal,'Desember', tahun);
+        break;
+
     default:
-        console.log('Data tidak dimasukkan')
+        console.log('Bulan tidak dimasukkan');
+        break;
 }
+
+
+
