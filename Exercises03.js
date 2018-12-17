7de6743bc4ef1254ec9004740759235d5fd59e93
basyri <husain.basyri@gmail.com>
2018-12-17 23:53:11 +0700

    Exercises03

---
+++ Exercises03.js
@@ -0,0 +1,20 @@
+var nama = 'husain'
+var peran = 'Ksatria'
+
+if (peran == ''){
+    console.log('Hallo', nama , ', Pilih peranmu untuk memulai game!')
+}
+else if(peran == 'Ksatria'){
+    console.log('Selamat datang di Dunia Proxytia, ', nama)
+    console.log("Halo Ksatria ", nama, ", kamu dapat menyerang dengan senjatamu!")
+}
+else if(peran == 'Tabib'){
+    console.log('Selamat datang di Dunia Proxytia, ', nama)
+    console.log("Halo Tabib ", nama, ", kamu akan membantu temanmu yang terluka.")
+}
+else{
+    console.log('Selamat datang di Dunia Proxytia, ', nama)
+    console.log("Halo Penyihir ", nama, ", ciptakan keajaiban yang membantu kemenanganmu!")
+}
+
+
