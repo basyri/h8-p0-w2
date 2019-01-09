812aa00cbbbc99fab87ec0cbd5cb339e47c686bd
basyri <husain.basyri@gmail.com>
2019-01-09 13:40:32 +0700

    update

--- Exercises03.js
+++ Exercises03.js
@@ -1,24 +1,19 @@
 var nama = 'Husain'
 var peran = 'Tabib'
-if(nama !== ''){
-	if (peran === ''){
-	    console.log('Hallo', nama , ', Pilih peranmu untuk memulai game!')
-	}
-	else if(peran === 'Ksatria'){
-	    console.log('Selamat datang di Dunia Proxytia, ', nama)
-	    console.log("Halo Ksatria ", nama, ", kamu dapat menyerang dengan senjatamu!")
-	}
-	else if(peran === 'Tabib'){
-	    console.log('Selamat datang di Dunia Proxytia, ', nama)
-	    console.log("Halo Tabib ", nama, ", kamu akan membantu temanmu yang terluka.")
-	}
-	else{
-	    console.log('Selamat datang di Dunia Proxytia, ', nama)
-	    console.log("Halo Penyihir ", nama, ", ciptakan keajaiban yang membantu kemenanganmu!")
+if (nama !== '') {
+	if (peran === '') {
+		console.log('Hallo', nama, ', Pilih peranmu untuk memulai game!')
+	} else if (peran === 'Ksatria') {
+		console.log('Selamat datang di Dunia Proxytia, ', nama)
+		console.log("Halo Ksatria ", nama, ", kamu dapat menyerang dengan senjatamu!")
+	} else if (peran === 'Tabib') {
+		console.log('Selamat datang di Dunia Proxytia, ', nama)
+		console.log("Halo Tabib ", nama, ", kamu akan membantu temanmu yang terluka.")
+	} else {
+		console.log('Selamat datang di Dunia Proxytia, ', nama)
+		console.log("Halo Penyihir ", nama, ", ciptakan keajaiban yang membantu kemenanganmu!")
 	}
 
-}else{
-   console.log('Nama harus di isi')	
+} else {
+	console.log('Nama harus di isi')
 }
-
-
--- Exercises06.js
+++ Exercises06.js
@@ -1,6 +1,6 @@
 console.log('LOOPING PERTAMA')
 var i = 2
-while(i <= 20){
+while (i <= 20) {
   console.log(i, " - I love coding")
   i += 2;
 }
@@ -8,75 +8,63 @@ while(i <= 20){
 console.log('---------------')
 console.log('LOOPING KEDUA')
 var i = 20
-while(i >= 2){
+while (i >= 2) {
   console.log(i, " - I love coding")
   i -= 2;
 }
 
 console.log('---------------')
 console.log('LOOPING PERTAMA FOR')
-for(var j = 1; j <= 20; j++){
-   console.log(j, " - I love coding")
+for (var j = 1; j <= 20; j++) {
+  console.log(j, " - I love coding")
 }
 
 console.log('---------------')
 console.log('LOOPING KEDUA FOR')
-for(var j = 20; j >= 1; j--){
-   console.log(j, " - I love coding")
+for (var j = 20; j >= 1; j--) {
+  console.log(j, " - I love coding")
 }
 
 console.log('---------------')
 console.log('LOOPING GANJIL GENAP')
-for(var j = 1; j <= 100; j++){
-  if(j % 2 == 0)
-   console.log('GEANAP')
-  else{
-   console.log('GANJIL')
+for (var j = 1; j <= 100; j++) {
+  if (j % 2 == 0)
+    console.log('GEANAP')
+  else {
+    console.log('GANJIL')
 
   }
 }
 
-console.log('---------------')
-console.log('LOOPING GANJIL GENAP')
-for(var j = 1; j <= 100; j++){
-  if(j % 2 == 0)
-   console.log('GEANAP')
-  else{
-   console.log('GANJIL')
-
-  }
-}
 
 console.log('---------------')
 console.log('LOOPING PERTAMBAHAN 2')
-for(var j = 1; j <= 100; j += 2){
-  if(j < 2){
-   console.log('')
-  }else{
-   console.log(j, 'KELIPATAN', j)
+for (var j = 1; j <= 100; j += 2) {
+  if (j % 3 === 0) {
+    console.log(j, 'KELIPATAN 3')
+  } else {
+    console.log('')
 
   }
 }
 
 console.log('---------------')
 console.log('LOOPING PERTAMBAHAN 5')
-for(var j = 1; j <= 100; j += 5){
-  if(j < 5){
-   console.log('')
-  }else{
-   console.log(j, 'KELIPATAN', j)
+for (var j = 1; j <= 100; j += 5) {
+  if (j % 6 === 0) {
+    console.log(j, 'KELIPATAN 6')
+  } else {
+    console.log('')
 
   }
 }
 
 console.log('---------------')
 console.log('LOOPING PERTAMBAHAN 9')
-for(var j = 1; j <= 100; j += 9){
-  if(j < 9){
-   console.log('')
-  }else{
-   console.log(j, 'KELIPATAN', j)
+for (var j = 1; j <= 100; j += 9) {
+  if (j % 10 === 0) {
+    console.log(j + ' KELIPATAN 10');
+  } else {
+    console.log('');
   }
 }
-
-
